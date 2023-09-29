095247957cda41b859f3b49a0f1ab80d94697fc3
vianeymm <vianeymaldonado6@gmail.com>
2023-09-29 15:37:31 -0500

    primer repositorio

---
+++ includes/footer.php
@@ -0,0 +1,57 @@
+    
+    <!-- ========== COMMON JS FILES ========== -->
+    <script src="assets/js/jquery/jquery-2.2.4.min.js"></script>
+    <script src="assets/js/jquery-ui/jquery-ui.min.js"></script>
+    
+    <script src="assets/js/pace/pace.min.js"></script>
+    <script src="assets/js/lobipanel/lobipanel.min.js"></script>
+    <script src="assets/js/iscroll/iscroll.js"></script>
+    <script src="assets/js/DataTables/datatables.min.js"></script>
+    <!-- ========== PAGE JS FILES ========== -->
+    <script src="assets/js/prism/prism.js"></script>
+    <script src="assets/js/waypoint/waypoints.min.js"></script>
+    <script src="assets/js/counterUp/jquery.counterup.min.js"></script>
+    <script src="assets/js/amcharts/amcharts.js"></script>
+    <script src="assets/js/amcharts/serial.js"></script>
+    <script src="assets/js/amcharts/plugins/export/export.min.js"></script>
+    <link rel="stylesheet" href="assets/js/amcharts/plugins/export/export.css" type="text/css" media="all" />
+    <script src="assets/js/amcharts/themes/light.js"></script>
+    <!-- <script src="assets/js/toastr/toastr.min.js"></script> -->
+    <script src="assets/js/icheck/icheck.min.js"></script>
+
+    <!-- ========== THEME JS ========== -->
+    <script src="assets/js/main.js"></script>
+    <script src="assets/js/production-chart.js"></script>
+    <script src="assets/js/traffic-chart.js"></script>
+    <script src="assets/js/task-list.js"></script>
+
+
+
+
+
+
+    <style>
+        .goog-logo-link {
+            display: none;
+        }
+
+        .goog-te-gadget {
+            color: white;
+        }
+
+        .goog-te-gadget .goog-te-combo {
+            margin: 0px 0;
+            padding: 8px;
+            color: white;
+        }
+
+        #google_translate_element {
+            padding-top: 11px;
+            color: white;
+
+            position: absolute;
+            right: 20px;
+        }
+    </style>
+
+   
---
+++ index.php
@@ -0,0 +1,70 @@
+<?php
+error_reporting(0);
+include('includes/config.php');
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Sistema de Calificaciones </title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="Free HTML Templates" name="keywords">
+  <meta content="Free HTML Templates" name="description">
+
+
+  <link href="./assets/images/favicon.ico" rel="icon">
+
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500;600;700&family=Open+Sans:wght@400;600&display=swap" rel="stylesheet">
+
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+
+  <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+  
+  <link href="./assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+  
+
+
+ 
+  <div class="container-fluid p-0">
+    <nav class="navbar navbar-expand-lg bg-white navbar-light py-3 py-lg-0 px-lg-5">
+      <a class="navbar-brand ml-lg-3">
+        <i class="fa fa-book-reader mr-3"></i>Universidad Tecnologíca de Poanas
+      </a>
+      <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-between px-lg-3" id="navbarCollapse">
+        <div class="navbar-nav mx-auto py-0">
+          <a href="index.html" class="nav-item nav-link active">Inicio</a>
+          <a href="#" class="nav-item nav-link">Nosotros</a>
+          <a href="#" class="nav-item nav-link">Cursos</a>
+          <div class="nav-item dropdown">
+            <div class="dropdown-menu m-0">
+            </div>
+          </div>
+        </div>
+        <a href="find-result.php" class="btn btn-primary py-2 px-4 d-none d-lg-block">Estudiantes</a>
+        <a href="admin-login.php" class="btn btn-primary py-2 px-4 d-none d-lg-block">Admin</a>
+      </div>
+    </nav>
+  </div>
+  
+  <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/waypoints/waypoints.min.js"></script>
+  <script src="lib/counterup/counterup.min.js"></script>
+  <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+  <script src="./assets/js/main.js"></script>
+</body>
+
+</html>
